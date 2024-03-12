a7f3f7753e7afd13c5751df6bb69f271aa1cfc10
aadamr0 <adamb.0@outlook.com>
2024-03-12 10:19:37 +0000

    posting comments functionality

--- src/utils.js
+++ src/utils.js
@@ -6,14 +6,14 @@ const ncNewsApi = axios.create({
 
 export function fetchArticles() {
   return ncNewsApi.get("/api/articles").then((response) => {
-    return response.data.articlesArray
+    return response.data.articlesArray;
   });
 }
 
 export function fetchArticleById(id) {
   return ncNewsApi.get(`/api/articles/${id}`).then((res) => {
-    return res.data
-  })
+    return res.data;
+  });
 }
 
 export function fetchCommentsByArticleId(id) {
@@ -23,5 +23,13 @@ export function fetchCommentsByArticleId(id) {
 }
 
 export function patchArticleById(id, vote) {
-  return ncNewsApi.patch(`/api/articles/${id}`, {inc_votes: vote})
+  return ncNewsApi.patch(`/api/articles/${id}`, { inc_votes: vote });
+}
+
+export function postCommentByArticleId(id, comment) {
+  return ncNewsApi.post(`/api/articles/${id}/comments`, {
+    username: comment.username,
+    body: comment.body,
+  })
+  .then((res) => res.data.commentObj)
 }
